da4b61059d5d6a28043e12ec01ea20d2bafacd8c
sundar <nagasundar.selvaraj@axesstechnologymail.in>
2024-05-16 17:10:23 +0530

    fixed footer on mobile responsiveness

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -229,8 +229,8 @@ export default function Main({ scrolled }) {
                             </div>
                         </div>
                         <footer className="absolute bottom-0 left-0 w-full">
-                            <div className="text-white container mx-auto tm-container p-8 text-lg flex flex-col md:flex-row justify-between">
-                                <span>Copyright 2022 Chennai Cafe. All rights reserved.</span>
+                            <div className="text-white bg-black bg-opacity-70 text-center text-lg py-2">
+                                <span>Copyright 2024 Chennai Cafe.<br/> All rights reserved.</span>
                                 {/* <span className="mt-5 md:mt-0">Design: <a href="https://www.tooplate.com" target="_parent">Tooplate</a></span> */}
                             </div>
                         </footer>

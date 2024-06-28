6663eeaac1b65c567241d3b8dc299d854d24439a
sangavi <sangavi.vijayaraghavan@axesstechnologymail.in>
2024-06-28 18:48:57 +0530

    new

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -230,7 +230,7 @@ export default function Main({ scrolled }) {
                         </div>
                         <footer className="absolute bottom-0 left-0 w-full">
                             <div className="text-white container mx-auto tm-container p-8 text-lg flex flex-col md:flex-row justify-between">
-                                <span>Copyright 2022 Chennai Cafe. All rights reserved.</span>
+                                <span>©2024 Chennai Cafe. All rights reserved.</span>
                                 {/* <span className="mt-5 md:mt-0">Design: <a href="https://www.tooplate.com" target="_parent">Tooplate</a></span> */}
                             </div>
                         </footer>
